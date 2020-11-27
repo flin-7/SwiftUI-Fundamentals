e12181d203e2508424525af5937522122a2b5e46
flin-7 <flin.7@icloud.com>
2020-11-27 01:32:03 -0800

    The Basics

---
+++ SwiftUI-Weather/SwiftUI-Weather/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  SwiftUI-Weather
+//
+//  Created by Felix Lin on 11/27/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.blue, .white]),
+                           startPoint: .topLeading,
+                           endPoint: .bottomTrailing)
+                .edgesIgnoringSafeArea(.all)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-Weather/SwiftUI-Weather/SwiftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherApp.swift
+//  SwiftUI-Weather
+//
+//  Created by Felix Lin on 11/27/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
