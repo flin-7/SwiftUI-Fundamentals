f5b93af0fc6ce1f45102f2818c1994ea5ad0483d
flin-7 <flin.7@icloud.com>
2020-11-29 23:32:51 -0800

    Button, Refactoring & Project Organization

---
+++ SwiftUI-Weather/SwiftUI-Weather/WeatherButton.swift
@@ -0,0 +1,23 @@
+//
+//  WeatherButton.swift
+//  SwiftUI-Weather
+//
+//  Created by Felix Lin on 11/29/20.
+//
+
+import SwiftUI
+
+struct WeatherButton: View {
+    var title: String
+    var textColor: Color
+    var backgroundColor: Color
+    
+    var body: some View {
+        Text(title)
+            .frame(width: 280, height: 50)
+            .background(backgroundColor)
+            .foregroundColor(textColor)
+            .font(.system(size: 20, weight: .bold))
+            .cornerRadius(10)
+    }
+}
