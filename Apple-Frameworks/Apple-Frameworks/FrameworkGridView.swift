801bdd6d72470747155a325f25ef22d4b648147c
flin-7 <flin.7@icloud.com>
2020-12-08 23:53:35 -0800

    Intro & Project Setup

---
+++ Apple-Frameworks/Apple-Frameworks/FrameworkGridView.swift
@@ -0,0 +1,20 @@
+//
+//  FrameworkGridView.swift
+//  Apple-Frameworks
+//
+//  Created by Felix Lin on 12/8/20.
+//
+
+import SwiftUI
+
+struct FrameworkGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FrameworkGridView_Previews: PreviewProvider {
+    static var previews: some View {
+        FrameworkGridView()
+    }
+}
