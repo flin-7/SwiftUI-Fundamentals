3be9a3a9f9f14c4a73bb940ee6c2373df9bc7cd4
flin-7 <flin.7@icloud.com>
2020-12-11 14:07:35 -0800

    Connecting Views - MVVM Introduction

---
+++ Apple-Frameworks/Apple-Frameworks/FrameworkGridViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  FrameworkGridViewModel.swift
+//  Apple-Frameworks
+//
+//  Created by Felix Lin on 12/11/20.
+//
+
+import SwiftUI
+
+final class FrameworkGridViewModel: ObservableObject {
+    var selectedFramework: Framework? {
+        didSet {
+            isShowingDetailView = true
+        }
+    }
+    
+    @Published var isShowingDetailView = false
+}
