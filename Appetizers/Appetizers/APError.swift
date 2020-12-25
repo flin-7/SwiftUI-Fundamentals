ee5b75f2a5149e96bcceb879a06a10d58896d9a0
flin-7 <flin.7@icloud.com>
2020-12-24 23:38:45 -0500

    NetworkManager

---
+++ Appetizers/Appetizers/APError.swift
@@ -0,0 +1,15 @@
+//
+//  APError.swift
+//  Appetizers
+//
+//  Created by Felix Lin on 12/24/20.
+//
+
+import Foundation
+
+enum APError: Error {
+    case invalidURL
+    case invalidResponse
+    case invalidData
+    case unableToComplete
+}
