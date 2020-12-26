84849b038a42e0197a6b2c00a45c92afa599f69f
flin-7 <flin.7@icloud.com>
2020-12-25 23:25:50 -0500

    Connecting our AppetizerListView to NetworkManager

---
+++ Appetizers/Appetizers/AppetizerListViewModel.swift
@@ -0,0 +1,26 @@
+//
+//  AppetizerListViewModel.swift
+//  Appetizers
+//
+//  Created by Felix Lin on 12/25/20.
+//
+
+import SwiftUI
+
+final class AppetizerListViewModel: ObservableObject {
+    
+    @Published var appetizers: [Appetizer] = []
+    
+    func getAppetizers() {
+        NetworkManager.shared.getAppetizers { result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let appetizers):
+                    self.appetizers = appetizers
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            }
+        }
+    }
+}
