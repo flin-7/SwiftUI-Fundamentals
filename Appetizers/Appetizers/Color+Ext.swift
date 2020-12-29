7e2a79d1879e7183a177df7be00e62cfe8533912
flin-7 <flin.7@icloud.com>
2020-12-29 12:24:45 -0800

    Download Image Network Call

---
+++ Appetizers/Appetizers/Color+Ext.swift
@@ -0,0 +1,16 @@
+//
+//  Color+Ext.swift
+//  Appetizers
+//
+//  Created by Felix Lin on 12/29/20.
+//
+
+import SwiftUI
+
+extension Color {
+    static let brandPrimary = Color("brandPrimary")
+}
+
+extension UIColor {
+    static let brandPrimary = UIColor(named: "brandPrimary")
+}
